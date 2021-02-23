bc7fdb24fb08a32526afa4788807f1a113e00b4d
antandant <404ari@gmail.com>
2021-02-23 10:00:00 +0900

    🐎  解答追加

---
+++ b/code_festival_qualA_b.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        int b = Integer.parseInt(br.readLine());
+        char ans;
+        if (b <= a.length()) {
+            ans = a.charAt(b - 1);
+        } else {
+            int mod = b % a.length();
+            ans = a.charAt(mod == 0 ? a.length() - 1 : mod - 1);
+        }
+        System.out.println(ans);
+    }
+}
