7401a49853f6cd6d2751070e751cc578c7de86db
wasshoy <404ari@gmail.com>
2021-04-14 23:12:44 +0900

    解答修正

--- a/arc105_a.java
+++ a/arc105_a.java
@@ -1,27 +1,24 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.StringBuffer;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long[] cookies = new long[4];
-        long sum = 0;
-        // 入力の受け取りと合計の計算
-        for (int i = 0; i < 4; i++) {
-            cookies[i] = sc.nextLong();
-            sum += cookies[i];
-        }
-        sc.close();
-
+    public static void main(String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] cokkies = Arrays.stream(br.readLine().split(" "))
+                       .mapToInt(Integer::parseInt)
+                       .toArray();
+        int sum = Arrays.stream(cokkies).sum();
         String ans = "No";
-        // 2**4 通りの組み合わせを試す
-        for (int i = 0; i < Math.pow(2, 4); i++) {
-            long eaten = 0;
-            for (int j = 0; j < 4; j++) {
-                if ((1 & i >> j) == 1) {
-                    eaten += cookies[j];
+        for (int i=0; i < (int)Math.pow(2, 4); i++) {
+            int eat = 0;
+            for (int j=0; j < 4; j++) {
+                if ((1&i >> j) == 1) {
+                    eat += cokkies[j];
                 }
             }
-            if (sum == 2 * eaten) {
+            if (sum == 2 * eat) {
                 ans = "Yes";
                 break;
             }
